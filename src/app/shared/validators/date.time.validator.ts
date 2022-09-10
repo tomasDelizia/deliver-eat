b7d0827ef101c917cef5c1a69d8c1a544dd0f890
Tomás Delizia <tomidelizia@gmail.com>
2022-09-09 21:39:04 -0300

    Arreglar función para obtener correctamente la fecha actual.

--- src/app/shared/validators/date.time.validator.ts
+++ src/app/shared/validators/date.time.validator.ts
@@ -1,11 +1,12 @@
 import { FormControl } from '@angular/forms';
+import * as moment from "moment";
 
 export class DateTimeValidator {
 
   static moreThanToday(control: FormControl): { [p: string]: any } | null {
     let date: string = control.value;
-    let today: string = new Date().toISOString().slice(0, 10);
-
+    let today: string = moment().format('YYYY-MM-DD')
+    console.log (date + ' ' + today)
     if (date < today)
       return { "moreThanToday": true };
 
